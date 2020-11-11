e025b6bd98314666c4cbc44aee03b354747fdaa5
Matthew Crowell <Matthew.Crowell1@gmail.com>
2020-11-10 22:31:06 -0600

    AK74, enemy AI, and firearm reticle added.

--- ZombieRunner/Assets/Scripts/EnemyAI.cs
+++ ZombieRunner/Assets/Scripts/EnemyAI.cs
@@ -6,8 +6,13 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
 	[SerializeField] Transform target;
+	[SerializeField] float chaseRange = 10f;
+	[SerializeField] float maxChaseRange = 30f;
+	[SerializeField] float attackRange = 1f;
 	
 	NavMeshAgent navMeshAgent;
+	float distanceToTarget = Mathf.Infinity;
+	bool isProvoked = false;
 	
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,46 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+	    distanceToTarget = Vector3.Distance(target.position, transform.position);
+		if(isProvoked)
+		{
+			EngageTarget();
+		}
+		else if(distanceToTarget <= chaseRange)
+		{
+			isProvoked = true;
+		}
+    }
+    
+    void OnDrawGizmosSelected()
+    {
+	    Gizmos.color = Color.red;
+	    Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+    
+   private void EngageTarget()
+    {
+	    // if in attack range, attack
+	    
+	    if(distanceToTarget <= navMeshAgent.stoppingDistance)
+	    {
+		    AttackTarget();
+	    }
+	    else if(distanceToTarget >= navMeshAgent.stoppingDistance)
+	    {
+		    ChaseTarget();
+	    }
+	    
+	    // distanceToTarget <= attackRange ? AttackTarget() : ChaseTarget();
+    }
+    
+    private void ChaseTarget()
+    {
+	    navMeshAgent.SetDestination(target.position);
+    }
+    
+	private void AttackTarget()
+    {
+	    Debug.Log(name + " is attacking " + target.name+ "!");
     }
 }
