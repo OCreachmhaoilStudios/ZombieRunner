e24f01ff3fe5a1ab4593d467567f7a69719e9f5a
Matthew Crowell <Matthew.Crowell1@gmail.com>
2020-11-11 02:55:13 -0600

    Animation controller, player health, etc, added.

---
+++ ZombieRunner/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	[SerializeField] float hitPoints = 100;
+	
+	// Create public method to reduce hitPoints by amount of damage specified by the weapon
+	
+	public void TakeDamage(float damage)
+	{
+		this.hitPoints -= damage;
+		Debug.Log(this.transform.name + " has " + this.hitPoints + " health remaining.");
+		if(this.hitPoints <= 0) { Destroy(gameObject); }
+		return;
+	}
+	
+}
---
+++ ZombieRunner/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+	[SerializeField] float maxHealth = 100f;
+	
+	public void TakeDamage(float damage)
+	{
+		/*
+		this.maxHealth -= damage;
+		if(this.maxHealth <= 0)
+		{
+			Debug.Log("Player has died.");
+		}
+		*/
+		if(this.maxHealth <= 0) { Debug.Log("Player is dead."); }
+		this.maxHealth = (this.maxHealth -= damage) >= 0 ? this.maxHealth - damage : 0;
+
+		return;
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
